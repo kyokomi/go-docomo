834c45eb9093ac025bf334a6dc41718270bdb19f
kyokomi <kyoko1220adword@gmail.com>
2014-12-21 10:40:47 +0900

    雑談APIのRequestを全部使えるようにした

--- zatudan.go
+++ zatudan.go
@@ -8,32 +8,38 @@ import (
 	"bytes"
 )
 
+const (
+	DIALOGUE_URL = "/dialogue/v1/dialogue"
+)
+
+// Mode dialog or srtr
+// CharactorID なし:デフォルト 20:関西弁 30:あかちゃん
+type DialogueRequest struct {
+	Utt            *string `json:"utt"`
+	Context        *string `json:"context"`
+	Nickname       *string `json:"nickname"`
+	NicknameYomi   *string `json:"nickname_y"`
+	Sex            *string `json:"sex"`
+	Bloodtype      *string `json:"bloodtype"`
+	BirthdateY     *int    `json:"birthdateY"`
+	BirthdateM     *int    `json:"birthdateM"`
+	BirthdateD     *int    `json:"birthdateD"`
+	Age            *int    `json:"age"`
+	Constellations *string `json:"constellations"`
+	Place          *string `json:"place"`
+	Mode           *string `json:"mode"`
+	CharactorID    *int    `json:"t"`
+}
+
 func (d *DocomoClient) SendZatsudan(nickname, message string) ([]byte, error) {
+	return d.sendZatsudan(DialogueRequest{
+		Utt:     &message,
+		Context: &d.context,
+		Nickname: &nickname,
+	})
+}
 
-	// Mode dialog or srtr
-	// CharactorID なし:デフォルト 20:関西弁 30:あかちゃん
-	type DialogueBody struct {
-		Utt            string `json:"utt"`
-		Context        string `json:"context"`
-		Nickname       string `json:"nickname"`
-//		NicknameYomi   string `json:"nickname_y"`
-//		Sex            string `json:"sex"`
-//		Bloodtype      string `json:"bloodtype"`
-//		BirthdateY     int    `json:"birthdateY"`
-//		BirthdateM     int    `json:"birthdateM"`
-//		BirthdateD     int    `json:"birthdateD"`
-//		Age            int    `json:"age"`
-//		Constellations string `json:"constellations"`
-//		Place          string `json:"place"`
-//		Mode           string `json:"mode"`
-//		CharactorID    int    `json:"t"`
-	}
-
-	b := DialogueBody{
-		Utt:     message,
-		Context: d.context,
-		Nickname: nickname,
-	}
+func (d *DocomoClient) sendZatsudan(b DialogueRequest) ([]byte, error) {
 
 	var data []byte
 	var err error
